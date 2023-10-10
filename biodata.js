fec2fbfbefb9ad9314c5eb2ca6a3d466400b4366
Flwrdirly <141025903+Flwrdirly@users.noreply.github.com>
2023-10-10 16:12:08 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,13 @@
+document.getElementById("text").innerHTML = "MY BIODATA";
+
+let nama = "Nama : Muhamad Dirly";
+document.getElementById("nama").innerHTML = nama ;
+
+let alamat = "Alamat : Cicurug ojo RT.03 RW.05";
+document.getElementById("alamat").innerHTML = alamat ;
+
+let ttg = "Tanggal Lahir : 25 january 2007";
+document.getElementById("ttg").innerHTML = ttg ;
+
+let hoby = "nyanyi dan dengerin musik";
+document.getElementById("hoby").innerHTML = hoby ;
