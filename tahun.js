c64006a9974d3c8204e1355c622e1a2c8533afdb
Flwrdirly <141025903+Flwrdirly@users.noreply.github.com>
2023-11-17 10:24:11 +0700

    Add files via upload

---
+++ tahun.js
@@ -0,0 +1,42 @@
+let pilihan = prompt("pilihan bulan")
+switch (pilihan) {
+  case 'january':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'february':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'maret':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'april':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'mei':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'junie':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'juli':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'agustus':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'september':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'oktober':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+  case 'november':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+    case 'desember':
+    document.write(`ini bulan ${pilihan}`);
+    break;
+    default:
+      document.writeln("anda salah memasukan bulan!")
+      break;
+}
